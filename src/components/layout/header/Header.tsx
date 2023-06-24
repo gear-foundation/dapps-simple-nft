8dee2b2aa8bbe76ffa6a007dfa6ddf5405b1590f
Nikita Yutanov <nikitayutanov@gmail.com>
2023-06-24 16:18:29 +0300

    Add home page responsive layout

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,3 +1,7 @@
+import { useState } from 'react';
+import { Button } from '@gear-js/ui';
+import { ReactComponent as HamburgerSVG } from 'assets/images/icons/hamburger.svg';
+import { ReactComponent as CrossSVG } from 'assets/images/icons/cross.svg';
 import { Wallet } from 'features/wallet';
 import { NodeSwitch } from 'features/node-switch';
 import { ContractAddress, useContractAddress } from 'features/contract-address';
@@ -9,21 +13,47 @@ import styles from './Header.module.scss';
 function Header() {
   const contractAddress = useContractAddress();
 
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => setIsMenuOpen((prevValue) => !prevValue);
+
   return (
     <header>
       <Container className={styles.container}>
         <Logo />
 
-        <div className={styles.wrapper}>
+        <div className={styles.mobileMenuWrapper}>
+          <Button
+            icon={isMenuOpen ? CrossSVG : HamburgerSVG}
+            color="transparent"
+            className={styles.button}
+            onClick={toggleMenu}
+          />
+
+          {isMenuOpen && (
+            <ul className={styles.list}>
+              <li className={styles.item}>
+                <ContractAddress />
+              </li>
+              <li className={styles.item}>
+                <NodeSwitch />
+              </li>
+            </ul>
+          )}
+        </div>
+
+        <div className={styles.configuration}>
           <Search />
 
-          <div className={styles.addresses}>
-            <ContractAddress />
-            {contractAddress && <span className={styles.separator} />}
-            <NodeSwitch />
-          </div>
+          <div className={styles.desktopMenu}>
+            <div className={styles.addresses}>
+              <ContractAddress />
+              {contractAddress && <span className={styles.separator} />}
+              <NodeSwitch />
+            </div>
 
-          <Wallet />
+            <Wallet />
+          </div>
         </div>
       </Container>
     </header>
