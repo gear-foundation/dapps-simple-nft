62e2b12ff97bb91d6c639f79dca8baf471d6ec57
Nikita Yutanov <nikitayutanov@gmail.com>
2023-07-20 17:27:24 +0300

    Minor fixes

--- src/features/nfts/components/transfer-nft-modal/TransferNFTModal.tsx
+++ src/features/nfts/components/transfer-nft-modal/TransferNFTModal.tsx
@@ -1,14 +1,17 @@
-import { useState, useMemo } from 'react';
+import { useMemo } from 'react';
 import { useParams } from 'react-router-dom';
-import { useSendMessage} from '@gear-js/react-hooks';
+import { useSendMessage } from '@gear-js/react-hooks';
 import { useForm } from '@mantine/form';
 import { useNFTs } from 'features/nfts/hooks';
 import { HexString } from '@polkadot/util/types';
 import { getProgramMetadata } from '@gear-js/api';
 import { Modal } from 'components';
-import styles from './TransferNFTModal.module.scss'
+import { isHex } from '@polkadot/util';
+import styles from './TransferNFTModal.module.scss';
 
-const initialValues = { to_address: '' };
+const initialValues = { address: '' };
+const validate = { address: (value: string) => (isHex(value) ? null : 'Address should be hex') };
+
 type Params = {
   programId: HexString;
   id: string;
@@ -19,42 +22,33 @@ type Props = {
 };
 
 export function TransferNFTModal({ onClose }: Props) {
-  const [, setNftFormForTrans] = useState(initialValues);
-
-  const resetForm = () => {
-    setNftFormForTrans(initialValues)
-  }
-  const {NFTContracts} = useNFTs();
+  const { NFTContracts } = useNFTs();
   const { programId, id } = useParams() as Params;
   const contract = NFTContracts.find(([address]) => address === programId);
   const metaRaw = contract?.[1];
   const metaHex = metaRaw ? (`0x${metaRaw}` as HexString) : undefined;
   const metadata = useMemo(() => (metaHex ? getProgramMetadata(metaHex) : undefined), [metaHex]);
 
-  const { getInputProps, onSubmit, errors } = useForm({ initialValues });
+  const { getInputProps, onSubmit, errors } = useForm({ initialValues, validate });
   const error = errors.address;
   const sendMessage = useSendMessage(programId, metadata);
 
-  const handleTransfer = onSubmit(async ({ to_address }) => {
-    // TODO: check the address for validity
-    const payload = {
-      Transfer: {
-          to: to_address,
-          nft_id: id,
-      }
-    };
-    sendMessage(payload, { onSuccess: resetForm });
-    onClose();
+  const handleTransfer = onSubmit((values) => {
+    const payload = { Transfer: { to: values.address, nft_id: id } };
+
+    sendMessage(payload, { onSuccess: onClose });
   });
 
   return (
     <Modal heading="Transfer NFT" onClose={onClose}>
       <form className={styles.form} onSubmit={handleTransfer}>
         <div>
-          
-          <span>To address</span>
-          {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-          <input placeholder="0x01" {...getInputProps('to_address')} className={styles.input} />
+          <input
+            placeholder="0x01"
+            /* eslint-disable-next-line react/jsx-props-no-spreading */
+            {...getInputProps('address')}
+            className={styles.input}
+          />
           {error && <p className={styles.error}>{error}</p>}
         </div>
 
@@ -65,4 +59,3 @@ export function TransferNFTModal({ onClose }: Props) {
     </Modal>
   );
 }
-
