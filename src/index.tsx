05716349e22daa4ed717e60330f3f65de1da3ae6
ereburg <gimmethenick@gmail.com>
2023-09-19 13:27:16 +0300

    add GTM analytics

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,21 @@
-import { StrictMode, useEffect } from 'react';
-import { createRoot } from 'react-dom/client';
-import 'keen-slider/keen-slider.min.css';
-import * as Sentry from '@sentry/react';
-import { createRoutesFromChildren, matchRoutes, useLocation, useNavigationType } from 'react-router-dom';
-import { App } from './app';
+import { StrictMode, useEffect } from 'react'
+import { createRoot } from 'react-dom/client'
+import 'keen-slider/keen-slider.min.css'
+import * as Sentry from '@sentry/react'
+import {
+  createRoutesFromChildren,
+  matchRoutes,
+  useLocation,
+  useNavigationType,
+} from 'react-router-dom'
+import TagManager from 'react-gtm-module'
+import { App } from './app'
+
+if (process.env.NODE_ENV === 'production' && process.env.REACT_APP_CB_GTM_ID) {
+  TagManager.initialize({
+    gtmId: process.env.REACT_APP_CB_GTM_ID,
+  })
+}
 
 Sentry.init({
   dsn: 'https://baf7a6def8eb0f06bd84f9007b257599@o4505867289100288.ingest.sentry.io/4505873662869504',
@@ -14,7 +26,7 @@ Sentry.init({
         useLocation,
         useNavigationType,
         createRoutesFromChildren,
-        matchRoutes,
+        matchRoutes
       ),
     }),
   ],
@@ -28,13 +40,13 @@ Sentry.init({
   // Session Replay
   replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-});
+})
 
-const container = document.getElementById('root');
-const root = createRoot(container as HTMLElement);
+const container = document.getElementById('root')
+const root = createRoot(container as HTMLElement)
 
 root.render(
   <StrictMode>
     <App />
-  </StrictMode>,
-);
+  </StrictMode>
+)
