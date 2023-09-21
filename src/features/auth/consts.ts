32b80a73e13366982732843bc6dc3b63d9ed000a
Yauheni <avegen.dr@gmail.com>
2023-09-21 11:42:58 +0300

    Updates meta and auth consts

--- src/features/auth/consts.ts
+++ src/features/auth/consts.ts
@@ -1,5 +1,5 @@
-export const AUTH_API_ADDRESS = process.env.REACT_APP_AUTH_API_ADDRESS as string;
+export const AUTH_API_ADDRESS = 'https://cb-campaign.vara-network.io'
 
-export const AUTH_TOKEN_LOCAL_STORAGE_KEY = 'authToken';
+export const AUTH_TOKEN_LOCAL_STORAGE_KEY = 'authToken'
 
-export const CB_UUID_KEY = 'cb_uiid';
+export const CB_UUID_KEY = 'cb_uiid'
--- src/features/auth/utils.ts
+++ src/features/auth/utils.ts
@@ -1,19 +1,12 @@
-import { AnyJson } from '@polkadot/types/types';
-import { AUTH_API_ADDRESS } from './consts';
-import { IApiError } from './types';
+import { AnyJson } from '@polkadot/types/types'
+import { AUTH_API_ADDRESS } from './consts'
+import { IApiError } from './types'
 
 export function trimEndSlash(url: string): string {
-  return url?.endsWith('/') ? url.slice(0, -1) : url;
+  return url?.endsWith('/') ? url.slice(0, -1) : url
 }
 
-export const API_URL = trimEndSlash(AUTH_API_ADDRESS);
-
-const post = (url: string, payload: AnyJson) =>
-  fetch(`${API_URL}/${url}`, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(payload),
-  });
+export const API_URL = trimEndSlash(AUTH_API_ADDRESS)
 
 const fetchAuth = <T>(url: string, method: string, payload?: AnyJson) =>
   fetch(`${API_URL}/${url}`, {
@@ -23,9 +16,9 @@ const fetchAuth = <T>(url: string, method: string, payload?: AnyJson) =>
     },
     body: payload ? JSON.stringify(payload) : undefined,
   }).then(async (response) => {
-    const json = await response.json();
-    if (!response.ok) throw new Error(await (json as IApiError).message);
-    return json as T;
-  });
+    const json = await response.json()
+    if (!response.ok) throw new Error(await (json as IApiError).message)
+    return json as T
+  })
 
-export { post, fetchAuth };
+export { fetchAuth }
